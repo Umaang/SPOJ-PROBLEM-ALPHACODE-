81834fccc5e00395120a5c892b80d4dd58162faf
Umaang <72022117+Umaang@users.noreply.github.com>
2021-05-13 21:38:04 +0530

    Add files via upload

--- oye.cpp
+++ oye.cpp
@@ -3,7 +3,7 @@ using namespace std;
 #define ll long long
 #define mod 1000000007
 const ll inf=1e18;
-ll dp[5000]; 
+ int dp[2001][2001]; 
  bool ispalindrome(string s){
     int i=0,j=s.length()-1;
     while(i<=j){
@@ -25,54 +25,28 @@ ll dp[5000];
      
  }
 void test(){
-  while(true){
-    string s;
-    cin>>s;
-    if(s=="0")
-      break;
-    int n=s.length();
-    memset(dp,0,sizeof(dp));
-    string res="";
-    dp[0]=1;
-    res.push_back(s[0]);
-    res.push_back(s[1]);
-    if(stoi(res)>26)
-      dp[1]=1;
-    else{
-      if(s[1]=='0')
-        dp[1]=1;
-      else
-        dp[1]=2;
+  int n;
+  cin>>n;
+  int v[n+1];
+  for(int i=1;i<=n;i++)
+    cin>>v[i];
+  memset(dp,-1,sizeof(dp));
+  for(int i=1;i<=n;i++){
+    for(int j=1;j<=n;j++){
+      if(i==j)
+        dp[i][i]=v[i]*n;
     }
-    for(int i=2;i<n;i++){
-       res="";
-      if(s[i]=='0')
-      {
-         dp[i]=dp[i-2];
-      }
-      else if(s[i-1]=='0')
-      {
-        dp[i]=dp[i-1];
-      }
-      else{
-      res.push_back(s[i-1]);
-      res.push_back(s[i]);
-      if(stoi(res)>26){
-        dp[i]=dp[i-1];
-      }
-      else{
-          dp[i]=dp[i-1]+dp[i-2];
-      }
-     }
-    }
-      
-     
-      cout<<dp[n-1]<<"\n";  
   }
- 
+  for(int i=n;i>=1;i--){
+    for(int j=i;j<=n;j++){
+      if(i!=j){
+        dp[i][j]=max(v[i]*(i-1+(n-j)+1)+dp[i+1][j],v[j]*(i-1+(n-j)+1)+dp[i][j-1]);
+      }
+    }
+  }
+  cout<<dp[1][n]<<"\n";
  //cout<<"Hello World"<<"\n";   
- 
-  //cout<<fixed<<setprecision(9)<<ans;
+ //cout<<fixed<<setprecision(9)<<ans;
 }
 signed main(){
     ios::sync_with_stdio(false);
